7dda5b6bd97fd1c44c574c691be5fb60d9a2e8b1
devcrack <aurelio.hdz@yandex.com>
2023-05-26 23:47:28 -0600

    Getting the greather common factor between 2 numbers

--- factors.py
+++ factors.py
@@ -9,5 +9,28 @@ def factors(num):
     return factors
 
 
+def gcf(number1, number2):
+    """Greatest common factor"""
+    if number2 > number1:
+        n1 = number2
+        n2 = number1
+    else:
+        n1 = number1
+        n2 = number2
+
+    factors_n1 = factors(n1)
+    factors_n2 = factors(n2)
+
+    gcf = None
+
+    for number in factors_n1:
+        if number in factors_n2:
+            gcf = number
+    return gcf
+
+
+    print()
+
 if __name__ == '__main__':
-    print(factors(15))
+    # print(factors(15))
+    print(gcf(150, 138))
